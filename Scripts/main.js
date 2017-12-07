8e8bc6a77096a3624a983639ac7497b70fdfbbc1
bensch <smapapurf@users.noreply.github.com>
2017-12-07 14:11:45 -0800

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,3 @@
-
 var c = document.getElementById("canvas").getContext("2d");
 var background=c.createPattern(document.getElementById("background"),"repeat");
 var pup=c.createPattern(document.getElementById("pup"),"repeat");
@@ -7,7 +6,35 @@ var pleft=c.createPattern(document.getElementById("pleft"),"repeat");
 var pright=c.createPattern(document.getElementById("pright"),"repeat");
 c.fillStyle=background;
 c.fillRect(0,0,500,500);
-c.fillStyle=pup;
-c.fillRect(0,0,50,50);
-c.fillStyle=pleft;
-c.fillRect(0,100,50,50);
+var player=new player();
+document.addEventListener("keypress", function(event)
+			{switch(event.which)
+					{
+					case 119:
+						console.log("press");
+						player.velocity[0]=0;
+						player.velocity[1]=-2;
+						break;
+					case 97:
+						console.log("press");
+						player.velocity[0]=-2;
+						player.velocity[1]=0;
+						break;
+					case 115:
+						console.log("press");
+						player.velocity[0]=0;
+						player.velocity[1]=2;
+						break;
+					case 100:
+						console.log("press");
+						player.velocity[0]=2;
+						player.velocity[1]=0;
+						break;
+					}});
+function loop()
+	{
+	c.fillStyle=background;
+	c.fillRect(0,0,500,500);
+	player.update();
+	}
+setInterval(loop,25);
